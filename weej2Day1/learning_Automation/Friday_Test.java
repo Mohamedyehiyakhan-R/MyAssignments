4fb3ca015a2e760f0186409ed60f2837fe790078
Mohamedyehiyakhan-R <yehiyakhan2004@gmail.com>
2025-07-18 12:50:09 +0530

    Add files via upload

---
+++ weej2Day1/learning_Automation/Friday_Test.java
@@ -0,0 +1,37 @@
+//package creation
+package learning_Automation;
+
+//imports
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+//class creation
+public class Friday_Test {
+	//declare main function
+	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions opt = new ChromeOptions();
+		//launch browser
+		ChromeDriver driver = new ChromeDriver(opt);
+		//upload URL
+		driver.get("https://www.saucedemo.com/v1/");
+		//Maximize the Window
+		driver.manage().window().maximize();
+		
+		//find elements in DOM page for user name
+		driver.findElement(By.id("user-name")).sendKeys("standard_user");
+		//use thread function for reducing time
+		Thread.sleep(2000);
+		
+		//find elements in DOM page for password
+		driver.findElement(By.id("password")).sendKeys("secret_sauce");
+		
+		//find elements in DOM page for login page
+		driver.findElement(By.id("login-button")).click();
+		
+		
+	}
+
+}
